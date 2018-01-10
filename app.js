0b04b37213bcff3bc15b49fc367889ee096a4fc4
jamesbarrett95 <james-barrett@hotmail.com>
2018-01-10 19:43:03 +0000

    Handling of new users and roles extracted from admin

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ require('dotenv').config({ path: 'variables.env' })
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
+const GoogleAuth = require('simple-google-openid')
 
 // create our Express app
 const app = express()
 
+// Serve up HTML and CSS from static folder
+app.use(express.static('static'))
+
 // Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// Google Client ID
+app.use(GoogleAuth(process.env.GOOGLE_CLIENT_ID))
+
+// return 'Not authorized' if we don't have a user
+app.use('/api', GoogleAuth.guardMiddleware({ realm: 'jwt' }))
 
 // Handle our routes after dealing with our middleware
 app.use('/', routes)
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,42 @@
 const express = require('express')
 const router = express.Router()
 
+const users = [{ email: 'up734253@myport.ac.uk', roles: ['user', 'admin'] }]
+
+function getCurrentLoggedIn (currentUser) {
+  const userExists = users.find(user => user.email === currentUser)
+  if (!userExists) {
+    const newUser = {
+      email: currentUser,
+      roles: []
+    }
+    return newUser
+  }
+  return userExists
+}
+
+router.get('/api/random', (req, res) => {
+  if (req.user.displayName) {
+    res.send(String(Math.random())) // Must be stringified otherwise Node will think it's a HTTP Status Code
+  } else {
+    res.send(res.sendStatus(403))
+  }
+
+  console.log('successful authenticated request by ' + req.user.emails[0].value)
+})
+
+router.get('/api/user/roles', (req, res) => {
+  const user = getCurrentLoggedIn(req.user.emails[0].value)
+  res.send(user.roles)
+})
+
+router.get('/api/user/request', (req, res) => {})
+router.post('/api/user/request', (req, res) => {})
+
+router.get('/api/user/approve', (req, res) => {})
+
+router.get('/api/users', (req, res) => {})
+
+router.delete('/api/user/:email')
+
 module.exports = router
